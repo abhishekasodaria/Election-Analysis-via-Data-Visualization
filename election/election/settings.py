94ead4df1293cfa4fabb417738f88b225b4ce1cb
abhishekwithfero <abhishek.asodaria@fero.ai>
2022-03-28 15:59:47 +0530

    Removed unwanted files

--- election/election/settings.py
+++ election/election/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from environ import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 Template_dir=os.path.join(BASE_DIR,'template')
